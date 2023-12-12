b2ca5933972a491924fcd5d636d92d57e3ff4bcb
Hafizfaturrohman <115616365+Hafizfaturrohman@users.noreply.github.com>
2023-12-12 19:51:45 +0700

    delete congfig.php

--- congfig.php
+++ congfig.php
@@ -1,9 +1,1 @@
-<?php
-// Config for database connection
-$config = array(
-    'host' => 'your_host',
-    'username' => 'your_username',
-    'password' => 'your_password',
-    'db_name' => 'your_database_name',
-);
-?>
+
